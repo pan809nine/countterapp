a97ab748123608a887f850c5333d08910c0b7cd6
Han's_Macbook <hans_macbook@Hans-Macbookde-MacBook-Pro.local>
2020-06-25 13:42:28 +0900

    1stcommit

---
+++ src/views/Count.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="count">
+        <p>{{ count }}</p>
+        
+            <button @click="increment()">+</button>
+            <button @click="decrement()">-</button>
+    </div>
+</template>
+
+<script>
+
+export default{
+    data(){
+        return{
+
+        }
+    },
+methods:{
+    increment(){
+        this.$store.commit('increment')
+    },
+    decrement(){
+        this.$store.commit('decrement')
+    }
+},
+computed:{
+    count(){
+        return this.$store.getters.count
+    }
+}
+}</script>
---
+++ src/views/Score.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>{{ count }}</p>
+    </div>
+</template>
+<script>
+export default {
+    computed:{
+        count(){
+            return this.$store.getters.count;
+        }
+    }
+}
+</script>
